f6b1fb5b8c7e5b65ce8225c7e61d14e8933332a5
ivan0961 <incivan_09@hotmail.com>
2016-08-29 00:58:47 +0300

    First commit

---
+++ DersSecimi/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace DersSecimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbSinif = new System.Windows.Forms.ComboBox();
+            this.lbDersler = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lbDersler);
+            this.groupBox1.Controls.Add(this.cbSinif);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(224, 293);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ÖĞRENCİ";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(88, 22);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(130, 23);
+            this.txtAd.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Adı Soyadı:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sınıfı:";
+            // 
+            // cbSinif
+            // 
+            this.cbSinif.FormattingEnabled = true;
+            this.cbSinif.Items.AddRange(new object[] {
+            ".NET",
+            "JAVA",
+            "ERP",
+            "SİSTEM",
+            "CISCO"});
+            this.cbSinif.Location = new System.Drawing.Point(88, 50);
+            this.cbSinif.Name = "cbSinif";
+            this.cbSinif.Size = new System.Drawing.Size(130, 24);
+            this.cbSinif.TabIndex = 3;
+            this.cbSinif.SelectedIndexChanged += new System.EventHandler(this.cbSinif_SelectedIndexChanged);
+            // 
+            // lbDersler
+            // 
+            this.lbDersler.FormattingEnabled = true;
+            this.lbDersler.ItemHeight = 16;
+            this.lbDersler.Location = new System.Drawing.Point(88, 80);
+            this.lbDersler.Name = "lbDersler";
+            this.lbDersler.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbDersler.Size = new System.Drawing.Size(130, 148);
+            this.lbDersler.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dersler:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(143, 253);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 29);
+            this.btnEkle.TabIndex = 6;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(18, 344);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(0, 13);
+            this.lblTest.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 369);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.ComboBox cbSinif;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbDersler;
+        private System.Windows.Forms.Label lblTest;
+    }
+}
+
